c573c4c9b7ac05086095d47a89a8dec84d38549d
Sziro06 <huoea071@gmail.com>
2023-09-11 10:58:41 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
 
 
 
 const pointsUI = document.querySelector("#points");
 let points = 0;
 
-const randomRangeNum = (max,min)=>{
+const randomRangeNum = (max,min) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const moveObstacles = (arr, speed, maxX, minX, maxZ, minZ) => {
+	arr.forEach((el) => {
+		el.body.position.z += speed;
+		if(el.body.position.z > camera.position.z) {
+			el.body.position.x = randomRangeNum(maxX, minX);
+			el.body.position.z = randomRangeNum(maxZ, minZ);
+		}
+		el.mesh.position.copy( el.body.position);
+		el.mesh.quaternion.copy( el.body.quaternion);
+	});
 }
 
 
 
 const scene = new THREE.Scene();
+const world = new CANNON.World({
+	gravity: new CANNON.Vec3(0, -9.82, 0)
+})
+const cannonDebugger = new CannonDebugger( scene, world, {
+	color: "#AEE2FF",
+	scale: 1,
+});
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 4.5;
 camera.position.y = 1.5;
@@ -28,6 +48,11 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
+const groundBody = new CANNON.Body({
+	shape: new CANNON.Box( new CANNON.Vec3( 15, 0.5, 15 ))
+})
+groundBody.position.y = -1
+world.addBody( groundBody );
 const ground = new THREE.Mesh(
 	new THREE.BoxGeometry( 30, 1, 30),
 	new THREE.MeshBasicMaterial( { color: 0x00ff00} )
@@ -36,7 +61,12 @@ ground.position.y = -1
 scene.add( ground );
 
 
-
+const playerBody = new CANNON.Body({
+	mass: 1,
+	shape: new CANNON.Box( new CANNON.Vec3( 0.25, 0.25, 0.25 )),
+	fixedRotation: true,
+});
+world.addBody( playerBody );
 const player = new THREE.Mesh(
 	new THREE.BoxGeometry( 0.5, 0.5, 0.5),
 	new THREE.MeshBasicMaterial( { color: 0xff0000} )
@@ -46,27 +76,53 @@ scene.add( player );
 
 const powerups = []
 for(let i = 0; i < 10; i++) {
+	const posX = randomRangeNum( 8,-8 );
+	const posZ = randomRangeNum( -5,-10 );
+
 	const powerup = new THREE.Mesh(
 		new THREE.TorusGeometry( 1, 0.4, 16, 50),
 		new THREE.MeshBasicMaterial({ color: 0xffff00 })
 	);
-	powerup.scale.set( 0.1, 0.1, 0.1 )
-	powerup.name = "powerup" + i + 1
-	powerup.position.x = i+2
-	powerups.push(powerup);
+	powerup.scale.set( 0.1, 0.1, 0.1 );
+	powerup.position.x = posX;
+	powerup.position.z = posZ;
+	powerup.name = "powerup" + [i + 1]
 	scene.add( powerup );
+
+	const powerupBody = new CANNON.Body({
+		shape: new CANNON.Sphere( 0.2 ),
+	});
+	powerupBody.position.set( posX, 0, posZ );
+	world.addBody( powerupBody );
+
+	const powerObject = {
+		mesh: powerup,
+		body: powerupBody,
+	};
+
+	powerups.push( powerObject );
+	
 }
 
 
-const gridHelper = new THREE.GridHelper( 30, 30 );
-scene.add( gridHelper)
+// const gridHelper = new THREE.GridHelper( 30, 30 );
+// scene.add( gridHelper)
 
 
 
 function animate() {
 	requestAnimationFrame( animate );
 
+	moveObstacles( powerups, 0.1, 8, -8, -5, -10)
+
 	controls.update();
+
+	world.fixedStep()
+
+	player.position.copy(playerBody.position);
+	player.quaternion.copy(playerBody.quaternion);
+
+	cannonDebugger.update()
 
 	renderer.render( scene, camera );
 }
@@ -83,15 +139,18 @@ window.addEventListener("resize",()=>{
 
 window.addEventListener("keydown",(e) => {
 	if(e.key === 'd' || e.key === 'D' || e.key === 'ArrowRight') {
-		player.position.x += 0.1;
-	}
-	if(e.key === 'a' || e.key === 'A' || e.key === 'ArrowLeft') {
-		player.position.x -= 0.1;
-	}
-	if(e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') {
-		player.position.z -= 0.1;
-	}
-	if(e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') {
-		player.position.z += 0.1;
+		playerBody.position.x += 0.1;
+	} else if(e.key === 'a' || e.key === 'A' || e.key === 'ArrowLeft') {
+		playerBody.position.x -= 0.1;
+	} else if(e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') {
+		playerBody.position.z -= 0.1;
+	} else if(e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') {
+		playerBody.position.z += 0.1;
+	} else if(e.key === 'p' || e.key === 'P'){
+		playerBody.position.x = 0;
+		playerBody.position.y = 0;
+		playerBody.position.z = 0;
+	} else if(e.key === ' '){
+		playerBody.position.y += 2;
 	}
   })
