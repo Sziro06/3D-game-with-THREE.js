4092e2bbe6123140cb7fca854a1640a129f60f8a
Sziro06 <huoea071@gmail.com>
2023-09-11 13:22:26 +0200

    Update
    I made this by watching a youtube tutorial and now I'm done watching. I need to fix something on the player movement, because the movement is not so smooth.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as CANNON from 'cannon-es';
 import CannonDebugger from 'cannon-es-debugger';
 
+// const gridHelper = new THREE.GridHelper( 30, 30 );
+// scene.add( gridHelper)
 
 
-const pointsUI = document.querySelector("#points");
+
+//variabeler
+const pointsUI = document.querySelector("#pointsUI");
 let points = 0;
+let gameOver = false;
 
+
+//randomRangeNum
 const randomRangeNum = (max,min) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -24,7 +31,7 @@ const moveObstacles = (arr, speed, maxX, minX, maxZ, minZ) => {
 }
 
 
-
+// lager scene, verden og tyndekraft.
 const scene = new THREE.Scene();
 const world = new CANNON.World({
 	gravity: new CANNON.Vec3(0, -9.82, 0)
@@ -48,6 +55,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
+// lager bakken
 const groundBody = new CANNON.Body({
 	shape: new CANNON.Box( new CANNON.Vec3( 15, 0.5, 15 ))
 })
@@ -61,6 +69,8 @@ ground.position.y = -1
 scene.add( ground );
 
 
+
+//lager hovedkarakteren
 const playerBody = new CANNON.Body({
 	mass: 1,
 	shape: new CANNON.Box( new CANNON.Vec3( 0.25, 0.25, 0.25 )),
@@ -74,6 +84,29 @@ const player = new THREE.Mesh(
 scene.add( player );
 
 
+
+// det som skjer når hovedkarakteren kolliderer
+playerBody.addEventListener("collide", (e) => {
+	powerups.forEach((el) => {
+		if( e.body === el.body ) {
+			el.body.position.x = randomRangeNum( 8,-8 );
+			el.body.position.z = randomRangeNum( -5,-10 );
+			el.mesh.position.copy( el.body.position );
+			el.mesh.quaternion.copy( el.body.quaternion );
+			points += 1;
+			pointsUI.textContent = points.toString();
+		}
+	});
+	enemies.forEach((el) => {
+		if( e.body === el.body ) {
+			gameOver = true
+		}
+	});
+})
+
+
+
+//poeng
 const powerups = []
 for(let i = 0; i < 10; i++) {
 	const posX = randomRangeNum( 8,-8 );
@@ -101,19 +134,102 @@ for(let i = 0; i < 10; i++) {
 	};
 
 	powerups.push( powerObject );
+}
+
+
+
+//fiender
+const enemies = [];
+
+for(let i = 0; i < 3; i++) {
+	const posX = randomRangeNum( 8,-8 );
+	const posZ = randomRangeNum( -5,-10 );
+
+	const enemy = new THREE.Mesh(
+		new THREE.BoxGeometry( 1, 1, 1 ),
+		new THREE.MeshBasicMaterial({ color: 0x0000ff })
+	);
+
+	enemy.position.x = posX;
+	enemy.position.z = posZ;
+	enemy.name = "enemy" + [i + 1];
+	scene.add( enemy );
+
+	const enemyBody = new CANNON.Body({
+		shape: new CANNON.Box( new CANNON.Vec3( 0.5, 0.5, 0.5 )),
+	});
+	enemyBody.position.set( posX, 0, posZ );
+	world.addBody( enemyBody );
+
+	const enemyObject = {
+		mesh: enemy,
+		body: enemyBody,
+	};
+
+	enemies.push( enemyObject );
 	
 }
 
 
-// const gridHelper = new THREE.GridHelper( 30, 30 );
-// scene.add( gridHelper)
+
+scene.fog = new THREE.FogExp2( 0x0047ab, 0.09, 50 );
+
+const geometry = new THREE.BufferGeometry();
+const vertices = [];
+const size = 2000;
+
+for( let i = 0; i < 5000; i++){
+	const x = (Math.random() * size + Math.random() * size) / 2 - size / 2;
+	const y = (Math.random() * size + Math.random() * size) / 2 - size / 2;
+	const z = (Math.random() * size + Math.random() * size) / 2 - size / 2;
+
+	vertices.push( x, y, z );
+}
+
+geometry.setAttribute(
+	"position",
+	new THREE.Float32BufferAttribute( vertices, 3 )
+);
+
+const material = new THREE.PointsMaterial({
+	size: 2,
+	color: 0xffffff,
+})
+
+const particles = new THREE.Points( geometry, material );
+scene.add( particles );
 
 
 
+//her er funksjonen som får de ulike tingene til å gjøre noe
 function animate() {
 	requestAnimationFrame( animate );
 
+	particles.rotation.x+=.0001
+	particles.rotation.y+=.0001
+	particles.rotation.z+=.0005
+
+	if(!gameOver){
 	moveObstacles( powerups, 0.1, 8, -8, -5, -10)
+	moveObstacles( enemies, 0.2, 8, -8, -5, -10)
+	} else {
+		pointsUI.textContent = "GAME OVER";
+		playerBody.velocity.set( playerBody.position.x, 5, 5 );
+
+		enemies.forEach((el) => {
+			scene.remove( el.mesh );
+			world.removeBody( el.body );
+		});
+		powerups.forEach((el) => {
+			scene.remove( el.mesh );
+			world.removeBody( el.body );
+		});
+
+		if( playerBody.position.z > camera.position.z ) {
+			scene.remove( player );
+			world.removeBody( playerBody );
+		}
+	}
 
 	controls.update();
 
@@ -131,12 +247,16 @@ animate();
 
 
 
+// koden til vinduet for at vinduet kan tilpasse seg til de forskjellige størrelsene til skjermen (responsiv).
 window.addEventListener("resize",()=>{
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
+
+
+// kontrollene
 window.addEventListener("keydown",(e) => {
 	if(e.key === 'd' || e.key === 'D' || e.key === 'ArrowRight') {
 		playerBody.position.x += 0.1;
